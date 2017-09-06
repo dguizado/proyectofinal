ae52db8ecef694fec091f8eb2bdc49d16d035227
dguizado <angigiacolemar@hotmail.com>
2017-09-05 22:53:49 -0500

    server y usb funcionalidades mejoradas

--- src/server.c
+++ src/server.c
@@ -4,16 +4,7 @@
 get_child(struct udev* udev, struct udev_device* parent, const char* subsystem)
 {
 
-    struct udev_device* child = NULL;
-    struct udev_enumerate *enumerate = udev_enumerate_new(udev);
-
    
-   if (udev_device == NULL)
-              return NULL;
-       if (!udev_device->parent_set) {
-
-	   udev_device->parent_set = 1;
-  
   udev_device->parent_device = device_new_from_parent(udev_device);
        }
        if (udev_device->parent_device != NULL)
@@ -22,11 +13,6 @@ get_child(struct udev* udev, struct udev_device* parent, const char* subsystem)
 
 			  return udev_device->parent_device;
     
-       char driver;
-
-       if (udev_device == NULL)
-
-	   return NULL;
 
 			  if (!udev_device->driver_set) {
 
@@ -50,17 +36,7 @@ struct udev *udev;
     struct udev_device *dev;
     
     
-    udev = udev_new();
-    if (!udev) {
-        
-        exit(1);
-    }
     
-    enumerate = udev_enumerate_new(udev);
-    udev_enumerate_add_match_subsystem(enumerate, "hidraw");
-    udev_enumerate_scan_devices(enumerate);
-    devices = udev_enumerate_get_list_entry(enumerate);
-   
    
     udev_list_entry_foreach(dev_list_entry, devices) {
         const char *path;
--- src/usb.c
+++ src/usb.c
@@ -15,41 +15,17 @@ static int pen_open(struct inode *i, struct file *f)
 {
     return 0;
 }
-static int pen_close(struct inode *i, struct file *f)
-{
-    return 0;
-}
 static ssize_t pen_read(struct file *f, char __user *buf, size_t cnt, loff_t *off)
 {
     int retval;
     int read_cnt;
- 
-    /* Read the data from the bulk endpoint */
-    retval = usb_bulk_msg(device, usb_rcvbulkpipe(device, BULK_EP_IN),
-            bulk_buf, MAX_PKT_SIZE, &read_cnt, 5000);
-    if (retval)
-    {
-        printk(KERN_ERR "Bulk message returned %d\n", retval);
-        return retval;
-    }
-    if (copy_to_user(buf, bulk_buf, MIN(cnt, read_cnt)))
-    {
-        return -EFAULT;
-    }
- 
-    return MIN(cnt, read_cnt);
+    return MIN(retval, read_cnt);
+    
 }
 static ssize_t pen_write(struct file *f, const char __user *buf, size_t cnt, loff_t *off)
 {
     int retval;
-    int wrote_cnt = MIN(cnt, MAX_PKT_SIZE);
- 
-    if (copy_from_user(bulk_buf, buf, MIN(cnt, MAX_PKT_SIZE)))
-    {
-        return -EFAULT;
-    }
- 
-    /* Write the data into the bulk endpoint */
+    
     retval = usb_bulk_msg(device, usb_sndbulkpipe(device, BULK_EP_OUT),
             bulk_buf, MIN(cnt, MAX_PKT_SIZE), &wrote_cnt, 5000);
     if (retval)
